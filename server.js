bb7457e1bf4f8ce76326ec3af03493b9f5b3ccb2
iYung <ivankhyung@gmail.com>
2019-02-27 21:22:33 -0500

    completed event post

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 3003;
-var user = express.Router();
-var vm = express.Router();
-var router = express.Router();
+var event = express.Router();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -33,56 +31,17 @@ app.use(function(req, res, next) {
     next();
 });
 
-router
-    .get("/", (req, res) => {
-        res.send("success");
-    })
-
-	// Sample post
-    .post("/test", (req, res) => {
-        res.send(req.body.paramname);
-    });
-
-user
-    .post("/login", (req, res) => {
-        connection.query("SELECT * FROM USERS WHERE USER_ID = '" + req.body.user + "' AND USER_PASSWORD = '" + req.body.password + "'", function (err, rows, fields) {
-            if (err) throw err
-            if (rows.length > 0) {
-                return res.json({success: true, id: rows[0].user_key});
+event
+    .post("/", (req, res) => {
+        connection.query(
+            "INSERT INTO EVENTS (CC_ID, VM_ID, EVENT_TIME, EVENT_TYPE, VM_TYPE) VALUES (" + req.body.cc_id + "," + req.body.vm_id + ", NOW(),'" + req.body.event_type + "','" + req.body.vm_type + "')", 
+                function (err, rows, fields) {
+                    if (err) throw err
+                    return res.json({success: true});
             }
-            return res.json({success: false});
-          })
-    })
-    .post("/charges", (req, res) => {
-        return res.json({success: false});
-    });
-
-vm
-    .post("/create", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/start", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/stop", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/delete", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/upgrade", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/downgrade", (req, res) => {
-        return res.json({success: false});
-    })
-    .post("/usage", (req, res) => {
-        return res.json({success: false});
+        )
     });
 
 // This will serve the webpage
-app.use(express.static("./src"));
-app.use("/api", router);
-app.use("/user", user);
-app.use("/vm", vm);
+app.use("/event", event);
 app.listen(port, () => console.log("Listening on port " + port));
